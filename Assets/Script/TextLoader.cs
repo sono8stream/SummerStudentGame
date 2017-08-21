08ace7ba7e988b2289092b6531a19adb750752cf
SONO <sono888.gamestudio@gmail.com>
2017-08-21 22:20:06 +0900

    条件分岐などもろもろ追加

--- Assets/Script/TextLoader.cs
+++ Assets/Script/TextLoader.cs
@@ -16,6 +16,8 @@ public class TextLoader : MonoBehaviour
     string message;
     [SerializeField]
     Transform choicesT, commandsT;
+    [SerializeField]
+    Image leftChara, rightChara;//キャラ画像
 
     int textLim = 2;//テキスト表示までのウェイト
     int tSetIndex;
@@ -32,6 +34,8 @@ public class TextLoader : MonoBehaviour
     Dictionary<string, int> regularExpression;
     Dictionary<string, IntVariable> variableDict;
     Predicate<string> messagePred;
+
+    int[] subVar;//予備変数
 
     // Use this for initialization
     void Start()
@@ -112,12 +116,25 @@ public class TextLoader : MonoBehaviour
                     case (int)RegularExpressions.着地点:
                         messagePred = (string t) => { return true; };
                         break;
-                    case (int)RegularExpressions.変数呼び出し:
+                    case (int)RegularExpressions.変数書き込み:
                         messagePred = ChangeVariable;
                         break;
-                    case (int)RegularExpressions.日付表示変更:
+                    case (int)RegularExpressions.変数取得:
+                        messagePred = GetVariable;
+                        break;
+                    case (int)RegularExpressions.状態表示変更:
                         messagePred = UpdateStatus;
                         break;
+                    case (int)RegularExpressions.乱数取得:
+                        messagePred = GetRandom;
+                        break;
+                    case (int)RegularExpressions.キャラ変更:
+                        messagePred = Highlight;
+                        break;
+                    case (int)RegularExpressions.条件判定:
+                        messagePred = CheckFlag;
+                        break;
+
                 }
             }
             else//普通にメッセージ表示
@@ -129,6 +146,7 @@ public class TextLoader : MonoBehaviour
 
     public void InitializeLoadMessage()
     {
+        subVar = new int[10];
         messageCacheList = Regex.Split(textSet.text, "\r\n|\r|\n");
         InitializeRE();
         InitializeVD();
@@ -149,7 +167,11 @@ public class TextLoader : MonoBehaviour
         regularExpression.Add("[j]", 6);
         regularExpression.Add("[a]", 7);
         regularExpression.Add("[h]", 8);
-        regularExpression.Add("[d]", 9);
+        regularExpression.Add("[g]", 9);
+        regularExpression.Add("[d]", 10);
+        regularExpression.Add("[n]", 11);
+        regularExpression.Add("[l]", 12);
+        regularExpression.Add("[f]", 13);
     }
 
     void InitializeVD()//変数辞典,[h]変数名:の形で指定可能
@@ -174,8 +196,11 @@ public class TextLoader : MonoBehaviour
     {
         if (textLim <= textCount)
         {
-            messageText.text += text[letterIndex];
-            letterIndex++;
+            if (!WriteVariable(text))//変数呼び出し
+            {
+                messageText.text += text[letterIndex];
+                letterIndex++;
+            }
             textCount = 0;
         }
         else
@@ -322,6 +347,30 @@ public class TextLoader : MonoBehaviour
         return true;
     }
 
+    bool GetVariable(string text)//[g]変数名:indexの形で指定したインデックスに読み込み
+    {
+        IntVariable var;
+        string tex = text.Split(']')[1];
+        string name = tex.Split(':')[0];
+
+        if (variableDict.ContainsKey(name))
+        {
+            var = variableDict[name];
+        }
+        else
+        {
+            return true;
+        }
+
+        tex = tex.Split(':')[1];
+        int index = int.Parse(tex.Substring(1));
+
+        if (index < 0 || subVar.Length <= index) { return true; }
+        subVar[index] = var.value;
+
+        return true;
+    }
+
     bool UpdateStatus(string text)//[d]
     {
         stamText.text = variableDict["体力"].value.ToString() + "/"
@@ -331,7 +380,117 @@ public class TextLoader : MonoBehaviour
         timeText.text = variableDict["時間"].value.ToString() + ":00";
         return true;
     }
+
+    bool GetRandom(string text)//[n]indexの形で指定したインデックスに乱数取得0~100
+    {
+        string t = text.Split(']')[1];
+        int index = int.Parse(t);
+
+        if (index < 0 || subVar.Length <= index) { return true; }
+        subVar[index] = UnityEngine.Random.Range(0, 100);
+        return true;
+    }
+    
+    bool Highlight(string text)
+    {
+        char c = text.Split(']')[1][0];
+        if(c=='l')
+        {
+            leftChara.color = Color.white;
+            rightChara.color = Color.gray;
+        }
+        else if(c=='r')
+        {
+            rightChara.color = Color.white;
+            leftChara.color = Color.gray;
+        }
+        else if(c=='n')
+        {
+            rightChara.color = Color.gray;
+            leftChara.color = Color.gray;
+        }
+
+        return true;
+    }
+
+    bool CheckFlag(string text)//[f], 分岐終点
+    {
+        string[] elem = text.Substring(3).Split(':');
+        int val1 = TextToVariable(elem[0]);
+        int val2 = TextToVariable(elem[2]);
+        char itr = elem[1][0];
+
+        bool flag = false;
+        switch (itr)
+        {
+            case '<':
+                flag = val1 < val2;
+                break;
+            case '>':
+                flag = val1 > val2;
+                break;
+            case '=':
+                flag = val1 == val2;
+                break;
+            case '≦':
+                flag = val1 <= val2;
+                break;
+            case '≧':
+                flag = val1 >= val2;
+                break;
+        }
+
+        if (flag)
+        {
+            JumpIndex("分岐終点");
+        }
+        Debug.Log(val2);
+        return true;
+    }
     #endregion
+
+    bool WriteVariable(string text)
+    {
+        if (text[letterIndex].Equals('[')
+            && letterIndex < text.Length - 1)//変数呼び出し
+        {
+            string t = text.Substring(letterIndex + 1).Split(']')[0];
+            if (variableDict.ContainsKey(t))
+            {
+                messageText.text += variableDict[t].value.ToString();
+                letterIndex += t.Length + 2;
+            }
+            else if (t.Length == 1 && 0 <= t[0] - '0' && t[0] - '0' <= 9)//予備変数
+            {
+                messageText.text += subVar[t[0] - '0'].ToString();
+                letterIndex += 3;
+            }
+            else
+            {
+                return false;
+            }
+        }
+        return false;
+    }
+
+    int TextToVariable(string text)
+    {
+        int val;
+
+        if (variableDict.ContainsKey(text))
+        {
+            val = variableDict[text].value;
+        }
+        else if (text.Length == 1 && 0 <= text[0] - '0' && text[0] - '0' <= 9)//予備変数
+        {
+            val = subVar[text[0] - '0'];
+        }
+        else
+        {
+            val = int.Parse(text);
+        }
+        return val;
+    }
 
     void SelectChoice(bool on)
     {
@@ -357,7 +516,8 @@ public enum RegularExpressions
 {
     イベントエンド = -1,
     キー待ち文字初期化, キー待ち, 文字初期化, 速度変更, 選択肢追加, 選択肢待ち,
-    ジャンプ, 着地点, 変数呼び出し, 日付表示変更
+    ジャンプ, 着地点, 変数書き込み, 変数取得, 状態表示変更, 乱数取得, キャラ変更,
+    条件判定
 }
 
 public enum VariableNames
